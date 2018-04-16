96f507674b04c094085f00db06aee41f7b3f8c7d
Evgenii Levinskii <el@ideal-code.ru>
2018-04-16 16:20:30 +0300

    Header minicart: render of item #9

--- app/design/frontend/IdealCode/DeepFish/Magento_Checkout/web/js/view/minicart.js
+++ app/design/frontend/IdealCode/DeepFish/Magento_Checkout/web/js/view/minicart.js
@@ -13,6 +13,17 @@ define([
         initialize: function() {
             this._super();
             this.cart = customerData.get('cart');
+        },
+
+        /**
+         * Remove price label, if there is only one price value
+         */
+        updatePrices: function() {
+            $('.minicart .prices').each(function() {
+                if($(this).find('> span').length <= 1) {
+                    $(this).find('> span').removeAttr('data-label');
+                }
+            });
         }
     });
 });
