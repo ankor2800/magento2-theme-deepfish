a1b0934ee6fc9e62549935628825c748ca23ab75
Evgenii Levinskii <el@ideal-code.ru>
2018-04-20 12:17:05 +0300

    Header minicart: quantity form #9

--- app/design/frontend/IdealCode/DeepFish/Magento_Checkout/web/js/view/minicart.js
+++ app/design/frontend/IdealCode/DeepFish/Magento_Checkout/web/js/view/minicart.js
@@ -1,28 +1,71 @@
 define([
+    'ko',
     'jquery',
     'uiComponent',
     'Magento_Customer/js/customer-data'
-], function($, Component, customerData) {
+], function(ko, $, Component, customerData) {
     'use strict';
 
     return Component.extend({
+        miniCart: $('.minicart'),
+        cart: customerData.get('cart'),
+        errorMessage: ko.observable(''),
 
         /**
          * @override
          */
         initialize: function() {
             this._super();
-            this.cart = customerData.get('cart');
+            this._setQuantityFormActions();
         },
 
         /**
          * Remove price label, if there is only one price value
          */
         updatePrices: function() {
-            $('.minicart .prices').each(function() {
+            this.miniCart.find('.prices').each(function() {
                 if($(this).find('> span').length <= 1) {
                     $(this).find('> span').removeAttr('data-label');
                 }
+            });
+        },
+
+        /**
+         * @private
+         */
+        _setQuantityFormActions: function() {
+            var self = this;
+
+            // Show submit link only if form is valid
+            this.miniCart.on('keyup', '.qty input', function() {
+                var $form = $(this).closest('form');
+                if($form.valid() && ($(this).val() != $(this).data('value'))) {
+                    $form.find('.submit').addClass('active');
+                } else {
+                    $form.find('.submit').removeClass('active');
+                }
+            });
+
+            this.miniCart.on('click', '.qty .submit', function() {
+                $(this).closest('form').submit();
+                return false;
+            });
+
+            // Ajax request if form is valid
+            this.miniCart.on('submit', '.qty', function() {
+                if($(this).valid()) {
+                    $.post($(this).attr('action'), $(this).serialize(), function(response) {
+                        self.errorMessage(response.error_message || '');
+                    }, 'json');
+                }
+                return false;
+            });
+
+            // Reset error messages and return input values
+            this.miniCart.on('dropdowndialogclose', function() {
+                self.errorMessage('').miniCart.find('.qty input').each(function() {
+                    $(this).val($(this).data('value')).valid();
+                });
             });
         }
     });
