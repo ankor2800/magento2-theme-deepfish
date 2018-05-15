0c79b1a91cc35c4c78eb86911dc7ccc7a9657a2c
Artem Korneychuk <ankor2800@gmail.com>
2018-05-15 11:17:41 +0300

    Banner widget #25

--- app/code/IdealCode/Banner/Model/ResourceModel/Banner.php
+++ app/code/IdealCode/Banner/Model/ResourceModel/Banner.php
@@ -3,11 +3,6 @@ namespace IdealCode\Banner\Model\ResourceModel;
 
 class Banner extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
-    /**
-     * Initialize resource model
-     *
-     * @return void
-     */
     protected function _construct()
     {
         $this->_init('ic_banner_items', 'id');
--- app/code/IdealCode/Banner/Model/ResourceModel/Banner/Collection.php
+++ app/code/IdealCode/Banner/Model/ResourceModel/Banner/Collection.php
@@ -3,11 +3,6 @@ namespace IdealCode\Banner\Model\ResourceModel\Banner;
 
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
-    /**
-     * Initialize resource collection
-     *
-     * @return void
-     */
     public function _construct()
     {
         $this->_init(
