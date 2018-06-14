57d150ab37bc2bf0577382c23f872087cb145a4e
Evgenii Levinskii <el@ideal-code.ru>
2018-06-14 17:21:53 +0300

    Remove unused scripts

---
+++ app/design/frontend/IdealCode/DeepFish/Magento_Theme/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Theme/js/responsive': 'mage/apply/main',
+            'Magento_Theme/js/theme': 'mage/apply/main'
+        }
+    }
+};
--- app/design/frontend/IdealCode/DeepFish/Magento_Theme/web/js/theme.js
+++
@@ -1,7 +0,0 @@
-/**
- * Override script from blank theme.
- * It's impossible not to include it, because it exists in deps section of requirejs.
- */
-define([], function() {
-    'use strict';
-});
