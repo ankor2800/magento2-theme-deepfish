23d953ccdf243dfa7b10ef261e9b8966bb79c554
Evgenii Levinskii <el@ideal-code.ru>
2018-06-26 10:33:44 +0300

    Created new modules #32

---
+++ app/code/DeepFish/CatalogRecentlyWidget/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'DeepFish_CatalogRecentlyWidget',
+    __DIR__
+);
