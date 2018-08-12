8b32119ddbf1d9e429027d6c5ab9b7cb7780384c
Artem Korneychuk <ankor2800@gmail.com>
2018-08-12 12:38:20 +0300

    Added new category attribute #40

--- app/code/DeepFish/Catalog/Override/Model/Category/DataProvider.php
+++ app/code/DeepFish/Catalog/Override/Model/Category/DataProvider.php
@@ -6,6 +6,8 @@ class DataProvider extends \Magento\Catalog\Model\Category\DataProvider
     protected function getFieldsMap()
     {
         $result = parent::getFieldsMap();
+
+        $result['general'][] = 'show_index_page';
         $result['content'][] = 'menu_description';
 
         return $result;
--- app/code/DeepFish/Catalog/Setup/InstallData.php
+++ app/code/DeepFish/Catalog/Setup/InstallData.php
@@ -1,6 +1,9 @@
 <?php
 namespace DeepFish\Catalog\Setup;
 
+use \Magento\Catalog\Model\Category;
+use \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
 class InstallData implements \Magento\Framework\Setup\InstallDataInterface
 {
     /** @var \Magento\Eav\Setup\EavSetupFactory */
@@ -22,12 +25,19 @@ class InstallData implements \Magento\Framework\Setup\InstallDataInterface
         /** @var \Magento\Eav\Setup\EavSetup $eavSetup */
         $eavSetup = $this->_eavSetupFactory->create(['setup' => $setup]);
 
-        $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'menu_description', [
+        $eavSetup->addAttribute(Category::ENTITY, 'menu_description', [
             'type' => 'text',
             'label' => 'Menu Description',
             'required' => false,
-            'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+            'global' => ScopedAttributeInterface::SCOPE_STORE,
             'wysiwyg_enabled' => true
+        ]);
+
+        $eavSetup->addAttribute(Category::ENTITY, 'show_index_page', [
+            'type' => 'int',
+            'label' => 'Show on Index page',
+            'required' => false,
+            'global' => ScopedAttributeInterface::SCOPE_STORE
         ]);
     }
 }
